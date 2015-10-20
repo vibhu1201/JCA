1c45bd5fb05e162ddcf8477b6c7ea930fe9edf84
Vaibhav Sharma <vibhu1201@gmail.com>
2015-10-19 23:10:24 -0500

    Fixed up home page. Changed fonts and mostly done with image carousel and News section. Added contact info in navbar

--- js/home.js
+++ js/home.js
@@ -1,9 +1,19 @@
 window.onload = (function() {
 
-  current_pic = $('.active');
-  current_pic.fadeOut(600).removeClass('active');
-  
+	window.setInterval(function(){
+  		current_pic = $('.active');
 
+		if(current_pic.next().length == 0) {
+			current_pic.fadeOut(800).removeClass('active').addClass('inactive').promise().done(function() {
+				$('.images img:first-child').fadeIn(800).addClass('active');
+			});	
+		}
+
+		else {
+			current_pic.fadeOut(800).removeClass('active').addClass('inactive').promise().done(function() {
+			current_pic.next().fadeIn(800).addClass('active');
+			});
+		}
+
+  	}, 5000);
 })();
-
-
